c4fdebb26f8cf613491018810bb03daacd3ad3f0
henrikeriksson33 <henrik.eriksson.brynas@gmail.com>
2024-11-21 15:43:36 +0100

    Förändring av #cities som finns i HTML Document
    Lägger till attribut till #cities för att ge den en prompt samt testar skriva annan övrig text för att se att detta funkar.

--- website/index.js
+++ website/index.js
@@ -4,3 +4,10 @@
 
 // Recommended: Ask for the city name and then the rest of the code
 
+
+let question = prompt ("Vilken stad?");
+
+const elementCities = document.querySelector(`#cities`);
+elementCities.innerHTML = `<h3>${question} (Frankrike) </h3>`;
+elementCities.style.justifyContent ="center";
+elementCities.style.fontSize = "25px";

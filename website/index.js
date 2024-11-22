033b9d1f5a18d0a7d6b6a3216931fb8b5cb09196
henrikeriksson33 <henrik.eriksson.brynas@gmail.com>
2024-11-22 16:44:43 +0100

    Flyttar Script i HTML
    Flyttar rangordningen på mina scripts i HTML dokumentet så att database fungerar på ett korrekt sätt.

--- website/index.js
+++ website/index.js
@@ -4,11 +4,16 @@
 
 // Recommended: Ask for the city name and then the rest of the code
 
+const main = document.querySelector("main");
+main.className = main;
+
 const elementFirstH2 = document.querySelector("h2");
 
 let question = prompt ("Vilken stad?");
+for (let i = 0; i<cities.length; i++)
 if (question == cities.name) {
-    elementFirstH2.textContent = `${cities.name} (${cities.country})`
+    elementFirstH2.textContent = `${foundCities.name} (${foundCities.country})`
+    document.title = `${question}`
 } else
     elementFirstH2.textContent = `${question} finns inte i databasen`;
     document.title = "Not found";
